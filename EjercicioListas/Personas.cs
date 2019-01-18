3077ef973cc3d9fee15848dd15c0962222474447
AdrianSanchezSegura <murciadri@gmail.com>
2019-01-18 09:29:15 +0100

    Agregar archivos de proyecto.

---
+++ EjercicioListas/Personas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioListas
+{
+    class Personas
+    {
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int telefono { get; set; }
+    }
+}
